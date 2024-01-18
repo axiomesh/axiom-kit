fdfeed1cdaccee2369885efbd174f61d2ac8509d
KRCheung <krcheung123@gmail.com>
2024-01-18 09:35:09 +0800

    feat(jmt): support verify trie

--- types/jmt.go
+++ types/jmt.go
@@ -2,49 +2,52 @@ package types
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"strconv"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/common"
+
 	"github.com/axiomesh/axiom-kit/hexutil"
 	"github.com/axiomesh/axiom-kit/types/pb"
-	"github.com/ethereum/go-ethereum/common"
 )
 
 type Node interface {
-	Encode() []byte
+	EncodePb() []byte
+	EncodeJson() []byte
 	GetHash() common.Hash
 	Copy() Node
-	Print() string // just for debug
+	String() string // just for debug
 }
 
 type (
 	InternalNode struct {
-		Children [16]*Child
+		Children [16]*Child `json:"children"`
 	}
 
 	LeafNode struct {
-		Key  []byte
-		Val  []byte
-		Hash common.Hash
+		Key  []byte      `json:"key"`
+		Val  []byte      `json:"value"`
+		Hash common.Hash `json:"hash"`
 	}
 
 	Child struct {
-		Hash    common.Hash
-		Version uint64
-		Leaf    bool
+		Hash    common.Hash `json:"hash"`
+		Version uint64      `json:"version"`
+		Leaf    bool        `json:"leaf"`
 	}
 )
 
-func (n *InternalNode) Encode() []byte {
-	blob, err := n.marshal()
+func (n *InternalNode) EncodeJson() []byte {
+	blob, err := json.Marshal(n)
 	if err != nil {
 		return nil
 	}
 	return blob
 }
 
-func (n *LeafNode) Encode() []byte {
-	blob, err := n.marshal()
+func (n *LeafNode) EncodeJson() []byte {
+	blob, err := json.Marshal(n)
 	if err != nil {
 		return nil
 	}
@@ -52,7 +55,7 @@ func (n *LeafNode) Encode() []byte {
 }
 
 // just for debug
-func (n *InternalNode) Print() string {
+func (n *InternalNode) String() string {
 	res := strings.Builder{}
 	for i := 0; i < 16; i++ {
 		res.WriteString(strconv.Itoa(i))
@@ -73,7 +76,7 @@ func (n *InternalNode) Print() string {
 }
 
 // just for debug
-func (n *LeafNode) Print() string {
+func (n *LeafNode) String() string {
 	res := strings.Builder{}
 	res.WriteString("Key[")
 	res.WriteString(hexutil.DecodeFromNibbles(n.Key))
@@ -86,8 +89,7 @@ func (n *LeafNode) Print() string {
 }
 
 func (n *InternalNode) GetHash() common.Hash {
-	d, _ := n.marshal()
-	data := sha256.Sum256(d)
+	data := sha256.Sum256(n.EncodePb())
 	return data
 }
 
@@ -98,8 +100,7 @@ func (n *LeafNode) GetHash() common.Hash {
 		Key:  n.Key,
 		Val:  n.Val,
 	}
-	d, _ := tmp.marshal()
-	data := sha256.Sum256(d)
+	data := sha256.Sum256(tmp.EncodePb())
 	return data
 }
 
@@ -118,9 +119,9 @@ func (n *LeafNode) Copy() Node {
 	return nn
 }
 
-func (n *InternalNode) marshal() ([]byte, error) {
+func (n *InternalNode) EncodePb() []byte {
 	if n == nil {
-		return nil, nil
+		return nil
 	}
 
 	children := make([]*pb.Child, 16)
@@ -141,31 +142,21 @@ func (n *InternalNode) marshal() ([]byte, error) {
 
 	content, err := blob.MarshalVTStrict()
 	if err != nil {
-		return nil, err
+		return nil
 	}
 
 	node := &pb.Node{
 		Leaf:    false,
 		Content: content,
 	}
-	return node.MarshalVTStrict()
-}
-
-func (n *InternalNode) fromPB(p *pb.InternalNode) {
-	n.Children = [16]*Child{}
-	for i, child := range p.Children {
-		if len(child.Hash) == 0 {
-			continue
-		}
-		n.Children[i] = &Child{
-			Hash:    common.BytesToHash(child.Hash),
-			Version: child.Version,
-			Leaf:    child.Leaf,
-		}
+	res, err := node.MarshalVTStrict()
+	if err != nil {
+		return nil
 	}
+	return res
 }
 
-func (n *InternalNode) unmarshal(data []byte) error {
+func (n *InternalNode) unmarshalInternalFromPb(data []byte) error {
 	helper := pb.InternalNodeFromVTPool()
 	defer func() {
 		helper.Reset()
@@ -175,40 +166,49 @@ func (n *InternalNode) unmarshal(data []byte) error {
 	if err != nil {
 		return err
 	}
-	n.fromPB(helper)
+
+	n.Children = [16]*Child{}
+	for i, child := range helper.Children {
+		if len(child.Hash) == 0 {
+			continue
+		}
+		n.Children[i] = &Child{
+			Hash:    common.BytesToHash(child.Hash),
+			Version: child.Version,
+			Leaf:    child.Leaf,
+		}
+	}
 	return nil
 }
 
-func (n *LeafNode) marshal() ([]byte, error) {
+func (n *LeafNode) EncodePb() []byte {
 	if n == nil {
-		return nil, nil
+		return nil
 	}
 
 	blob := &pb.LeafNode{
-		Key:   hexToBytes(n.Key),
+		Key:   HexToBytes(n.Key),
 		Value: n.Val,
 		Hash:  n.Hash[:],
 	}
 
 	content, err := blob.MarshalVTStrict()
 	if err != nil {
-		return nil, err
+		return nil
 	}
 
 	node := &pb.Node{
 		Leaf:    true,
 		Content: content,
 	}
-	return node.MarshalVTStrict()
+	res, err := node.MarshalVTStrict()
+	if err != nil {
+		return nil
+	}
+	return res
 }
 
-func (n *LeafNode) fromPB(p *pb.LeafNode) {
-	n.Val = p.Value
-	n.Key = bytesToHex(p.Key)
-	n.Hash = common.BytesToHash(p.Hash)
-}
-
-func (n *LeafNode) unmarshal(data []byte) error {
+func (n *LeafNode) unmarshalLeafFromPb(data []byte) error {
 	helper := pb.LeafNodeFromVTPool()
 	defer func() {
 		helper.Reset()
@@ -218,11 +218,13 @@ func (n *LeafNode) unmarshal(data []byte) error {
 	if err != nil {
 		return err
 	}
-	n.fromPB(helper)
+	n.Val = helper.Value
+	n.Key = BytesToHex(helper.Key)
+	n.Hash = common.BytesToHash(helper.Hash)
 	return nil
 }
 
-func UnmarshalJMTNode(data []byte) (Node, error) {
+func UnmarshalJMTNodeFromPb(data []byte) (Node, error) {
 	if len(data) == 0 {
 		return nil, nil
 	}
@@ -238,7 +240,7 @@ func UnmarshalJMTNode(data []byte) (Node, error) {
 
 	if helper.Leaf {
 		res := &LeafNode{}
-		err = res.unmarshal(helper.Content)
+		err = res.unmarshalLeafFromPb(helper.Content)
 		if err != nil {
 			return nil, err
 		}
@@ -247,7 +249,7 @@ func UnmarshalJMTNode(data []byte) (Node, error) {
 
 	if !helper.Leaf {
 		res := &InternalNode{}
-		err = res.unmarshal(helper.Content)
+		err = res.unmarshalInternalFromPb(helper.Content)
 		if err != nil {
 			return nil, err
 		}
@@ -257,42 +259,73 @@ func UnmarshalJMTNode(data []byte) (Node, error) {
 	return nil, nil
 }
 
-// bytesToHex expand normal bytes to hex bytes (nibbles)
-func bytesToHex(h []byte) []byte {
+func UnmarshalJMTNodeFromJson(rawNode []byte) (n Node, err error) {
+	if len(rawNode) == 0 {
+		return nil, nil
+	}
+	m := make(map[string]any)
+	if err := json.Unmarshal(rawNode, &m); err != nil {
+		return nil, err
+	}
+	if m["children"] != nil {
+		return unmarshalInternalFromJson(rawNode)
+	} else if m["key"] != nil {
+		return unmarshalLeafFromJson(rawNode)
+	}
+	return nil, nil
+}
+
+func unmarshalInternalFromJson(rawNode []byte) (Node, error) {
+	n := &InternalNode{}
+	err := json.Unmarshal(rawNode, n)
+	if err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+func unmarshalLeafFromJson(rawNode []byte) (Node, error) {
+	n := &LeafNode{}
+	err := json.Unmarshal(rawNode, n)
+	if err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+// BytesToHex expand normal bytes to hex bytes (nibbles)
+func BytesToHex(h []byte) []byte {
 	if len(h) == 0 {
 		return h
 	}
 	var i byte = 0
-	length := h[0]
-	tmp := h[1:]
+	length := len(h) * 2
 	dst := make([]byte, length)
-	for ; i < length; i++ {
+	for ; int(i) < length; i++ {
 		if i&0x01 == 0 {
-			dst[i] = tmp[i/2] >> 4
+			dst[i] = h[i/2] >> 4
 		} else {
-			dst[i] = tmp[i/2] & 15
+			dst[i] = h[i/2] & 15
 		}
 	}
 	return dst
 }
 
-// hexToBytes compress hex bytes (also called nibbles) to normal bytes
-func hexToBytes(src []byte) []byte {
+// HexToBytes compress hex bytes (also called nibbles) to normal bytes
+func HexToBytes(src []byte) []byte {
 	if len(src) == 0 {
 		return src
 	}
 	if len(src) > 255 {
 		panic("don't support compress bytes with length greater than 255 ")
 	}
-	length := (1 + len(src)) / 2
-	res := make([]byte, 1+length)
-	res[0] = byte(len(src))
+	if len(src)%2 != 0 {
+		panic("don't support compress bytes with odd length")
+	}
+	length := len(src) / 2
+	res := make([]byte, length)
 	for i := 0; i < length; i++ {
-		if 2*i+1 == len(src) {
-			res[i+1] = src[2*i] << 4
-		} else {
-			res[i+1] = (src[2*i] << 4) | src[2*i+1]
-		}
+		res[i] = (src[2*i] << 4) | src[2*i+1]
 	}
 	return res
 }
