3ce1d48531a63ddfb8cfc3cee69da764d59a6c46
Ruoxin <karen_lrx@163.com>
2024-01-19 19:01:31 +0800

    fix: file utils function

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -4,8 +4,26 @@ import "os"
 
 // Exist check if the file with the given path exits.
 func Exist(path string) bool {
+	_, err := os.Lstat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+
+	return true
+}
+
+func ExistDir(path string) bool {
 	fi, err := os.Lstat(path)
-	if fi != nil || (err != nil && !os.IsNotExist(err)) {
+	if err != nil {
+		if os.IsExist(err) && fi != nil && fi.IsDir() {
+			return true
+		}
+		return false
+	}
+	if fi != nil && fi.IsDir() {
 		return true
 	}
 
